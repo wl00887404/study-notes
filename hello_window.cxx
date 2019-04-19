8f0129328f11187dab21326bd5f61d8637d791ed
wl00887404 <wl00887404@gmail.com>
2019-04-19 21:36:09 +0800

    3.33 teapotRender.cpp

--- hello_window.cxx
+++ hello_window.cxx
@@ -1,39 +1,53 @@
+#include <iostream>
 #include <GL/glut.h>
+using namespace std;
 
-void init();
 void display();
+void reshape(int width, int height);
+
+float backgroundGray = 1.0f;
+float teapot_size = 1.0f;
 
 int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(300, 300);
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("OpenGL 3D View");
+  glutInitWindowPosition(100, 100); //設定初始化視窗位置
+  glutInitWindowSize(600, 600);     //設定初始化視窗大小
 
-    init();
-    glutDisplayFunc(display);
+  glutCreateWindow("OpenGL 3D View");                                 //創立一個GLUT視窗
+  glClearColor(backgroundGray, backgroundGray, backgroundGray, 1.0f); //設定清除顏色
 
-    glutMainLoop();
-    return 0;
-}
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape); //註冊視窗重塑事件
 
-void init()
-{
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(-5, 5, -5, 5, 5, 15);
-    glMatrixMode(GL_MODELVIEW);
-    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+  glutMainLoop();
+  return 0;
 }
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor(backgroundGray, backgroundGray, backgroundGray, 1.0f);
 
-    glColor3f(1.0, 0, 0);
-    glutWireTeapot(3);
+  glMatrixMode(GL_MODELVIEW); //指定目前運算的矩陣為視覽座標矩陣及模塑座標矩陣
+  glLoadIdentity();
+  gluLookAt(0.0, 2.0, 5.0,
+            0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0); //設定相機的位置，視點，y軸向量
 
-    glFlush();
+  glColor3b(125, 0, 0);        //設定目前所使用的顏色
+  glutWireTeapot(teapot_size); //畫出線框茶壺
+
+  glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+  float aspect = width * 1.0f / height;
+  glMatrixMode(GL_PROJECTION);                //指定目前運算的矩陣為投影矩陣
+  glLoadIdentity();                           //將目前指定的矩陣重設為單位矩陣
+  glViewport(0, 0, width, height);            //視埠，將投影影像對應在視窗座標上
+  gluPerspective(60.0f, aspect, 0.1f, 10.0f); //投影模式為透視，並設定其參數
 }
