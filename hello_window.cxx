ce4f4bbe5f8d299c648a458790f60fd9ff4e9fe9
wl00887404 <wl00887404@gmail.com>
2019-05-06 17:06:20 +0800

    hello_window keyboard event

--- hello_window.cxx
+++ hello_window.cxx
@@ -4,9 +4,12 @@ using namespace std;
 
 void display();
 void reshape(int width, int height);
+void keyboard(unsigned char key, int x, int y);
 
 float backgroundGray = 1.0f;
 float teapot_size = 1.0f;
+float teapot_posX = 0.0f;
+float teapot_posY = 0.0f;
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +24,7 @@ int main(int argc, char *argv[])
 
   glutDisplayFunc(display);
   glutReshapeFunc(reshape); //註冊視窗重塑事件
+  glutKeyboardFunc(keyboard);
 
   glutMainLoop();
   return 0;
@@ -33,12 +37,13 @@ void display()
 
   glMatrixMode(GL_MODELVIEW); //指定目前運算的矩陣為視覽座標矩陣及模塑座標矩陣
   glLoadIdentity();
-  gluLookAt(0.0, 2.0, 5.0,
+  gluLookAt(0.0f, 2.0f, 5.0,
             0.0, 0.0, 0.0,
             0.0, 1.0, 0.0); //設定相機的位置，視點，y軸向量
 
-  glColor3b(125, 0, 0);        //設定目前所使用的顏色
-  glutWireTeapot(teapot_size); //畫出線框茶壺
+  glTranslatef(teapot_posX, teapot_posY, 0.0f); //將目前的矩陣乘上位移矩陣
+  glColor3b(125, 0, 0);                         //設定目前所使用的顏色
+  glutWireTeapot(teapot_size);                  //畫出線框茶壺
 
   glutSwapBuffers();
 }
@@ -50,4 +55,26 @@ void reshape(int width, int height)
   glLoadIdentity();                           //將目前指定的矩陣重設為單位矩陣
   glViewport(0, 0, width, height);            //視埠，將投影影像對應在視窗座標上
   gluPerspective(60.0f, aspect, 0.1f, 10.0f); //投影模式為透視，並設定其參數
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+  if (key == 'W' || key == 'w')
+  {
+    teapot_posY += 0.05f;
+  }
+  else if (key == 'S' || key == 's')
+  {
+    teapot_posY -= 0.05f;
+  }
+  else if (key == 'A' || key == 'a')
+  {
+    teapot_posX -= 0.05f;
+  }
+  else if (key == 'D' || key == 'd')
+  {
+    teapot_posX += 0.05f;
+  }
+
+  glutPostRedisplay();
 }
