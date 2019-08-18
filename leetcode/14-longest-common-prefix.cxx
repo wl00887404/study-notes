3002576a9a700703d31700b1b2c9615063cbde3a
wl00887404 <wl00887404@gmail.com>
2019-08-18 15:50:06 +0800

    2019/8/18

---
+++ leetcode/14-longest-common-prefix.cxx
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  string longestCommonPrefix(vector<string> &strs)
+  {
+    if (strs.size() == 0)
+    {
+      return "";
+    }
+
+    if (strs.size() == 1)
+    {
+      return strs[0];
+    }
+
+    const char *pointers[strs.size() - 1];
+    const char *ref = strs[0].c_str();
+    string result = "";
+
+    for (int i = 0; i < strs.size() - 1; i++)
+    {
+      pointers[i] = strs[i + 1].c_str();
+    }
+
+    while (*ref != '\0')
+    {
+      for (int i = 1; i < strs.size(); i++)
+      {
+        if (*pointers[i - 1] != *ref)
+        {
+          return result;
+        }
+
+        pointers[i - 1]++;
+      }
+
+      result += *ref;
+      ref++;
+    }
+    return result;
+  }
+} solution;
+
+int main()
+{
+  vector<string> strs = {"a", "b"};
+
+  cout << solution.longestCommonPrefix(strs) << endl;
+}
---
+++ leetcode/50-pow(x, n).cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Solution
+{
+public:
+  double myPow(double x, int n)
+  {
+    return pow(x, n);
+  }
+};
