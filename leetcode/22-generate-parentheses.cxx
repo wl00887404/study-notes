3372a4b05c2bcd7ba33874797ebca5e50034ac00
wl00887404 <wl00887404@gmail.com>
2019-08-30 22:27:23 +0800

    2019/8/24

--- leetcode/22-generate-parentheses.cxx
+++ leetcode/22-generate-parentheses.cxx
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
-
-// 未完成
 
 /*
 * 1: (1)
 * 2: (1, 1)
-* 3: (1, 2) (2, 1)
-* 4: (1, 3) (2, 2) (3, 1)
-* 5: (1, 4) (2, 3) (3, 2) (4, 1)
+* 3: (1, 2)
+* 4: (1, 3) (2, 2)
+* 5: (1, 4) (2, 3)
 */
 
 unordered_map<int, vector<string>> cache = {
@@ -24,18 +23,32 @@ public:
     if (cache.count(n))
       return cache[n];
 
-    vector<string> parenthesis = generateParenthesis(n - 1);
+    unordered_set<string> set;
     vector<string> result;
 
-    // for (string str : parenthesis)
-    //   result.push_back("(" + str + ")");
+    for (string str : generateParenthesis(n - 1))
+      set.insert("(" + str + ")");
 
-    // for (string str : parenthesis)
-    // {
-    //   if (!(str[0] == '(' && str[1] == ')'))
-    //     result.push_back(str + "()");
-    //   result.push_back("()" + str);
-    // }
+    for (int i = 1; i <= n - i; i++)
+    {
+      cout << n << ": (" << i << ", " << n - i << ")" << endl;
+      vector<string> p1 = generateParenthesis(i);
+      vector<string> p2 = generateParenthesis(n - i);
+
+      for (string prefix : p1)
+      {
+        for (string str : p2)
+        {
+          set.insert(str + prefix);
+          set.insert(prefix + str);
+        }
+      }
+    }
+
+    for (string str : set)
+      result.push_back(str);
+
+    cache[n] = result;
 
     return result;
   }
@@ -43,9 +56,14 @@ public:
 
 int main()
 {
-  vector<string> result = solution.generateParenthesis(1);
+  vector<string> result = solution.generateParenthesis(4);
+
+  cout << result.size() << endl;
 
   for (string str : result)
+  {
     cout << str << endl;
+  }
+
   return 0;
 }
