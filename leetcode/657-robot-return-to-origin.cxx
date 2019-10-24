c8e7759eecda815ff1274c728c93bef1ce6c1f30
wl00887404 <wl00887404@gmail.com>
2019-10-24 13:39:11 +0800

    657: 0 鑑別度

---
+++ leetcode/657-robot-return-to-origin.cxx
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * https://medium.com/i-am-mike/javascript-%E7%9A%84-leetcode-%E6%BC%94%E7%AE%97%E4%B9%8B%E8%B7%AF-%E4%BA%8C-e4ce3f513c67
+ * 這樣也一篇
+ */
+
+class Solution {
+ public:
+  bool judgeCircle(string moves) {
+    int x = 0;
+    int y = 0;
+
+    for (const char* p = moves.c_str(); *p != '\0'; p++) {
+      switch (*p) {
+        case 'R':
+          x++;
+          break;
+        case 'L':
+          x--;
+          break;
+        case 'U':
+          y++;
+          break;
+        case 'D':
+          y--;
+          break;
+      }
+    }
+
+    return x == 0 && y == 0;
+  }
+};
