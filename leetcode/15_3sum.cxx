1361b0df2af7e776b3c0722c7f98a4be580a83a5
wl00887404 <wl00887404@gmail.com>
2020-04-24 23:32:32 +0800

    49: 質數 hash 法

--- leetcode/15_3sum.cxx
+++ leetcode/15_3sum.cxx
@@ -6,7 +6,11 @@ using namespace std;
 /**
  * 混在一起 352 ms
  * 分成正負 256 ms
- *
+ * 改掉 unordered_map::begin() 196 ms
+ * 
+ * 太多元素會降低 unordered_map 的速度
+ * boolean[] 試試看
+ * 
  * TODO: 敢不敢再快一點
  */
 
@@ -19,7 +23,9 @@ class Solution {
     if (length < 3) return results;
 
     unordered_map<int, int> positiveMap;
+    vector<int> positiveNums;
     unordered_map<int, int> negativeMap;
+    vector<int> negativeNums;
     int zeroLength = 0;
 
     for (int i = 0; i < length; i++) {
@@ -29,99 +35,82 @@ class Solution {
       }
 
       if (nums[i] > 0) {
-        if (positiveMap.count(nums[i])) {
-          positiveMap[nums[i]]++;
-        } else {
-          positiveMap[nums[i]] = 1;
+        if (!positiveMap.count(nums[i])) {
+          positiveMap[nums[i]] = 0;
+          positiveNums.push_back(nums[i]);
         }
+
+        positiveMap[nums[i]]++;
       } else {
-        if (negativeMap.count(nums[i])) {
-          negativeMap[nums[i]]++;
-        } else {
-          negativeMap[nums[i]] = 1;
+        if (!negativeMap.count(nums[i])) {
+          negativeMap[nums[i]] = 0;
+          negativeNums.push_back(nums[i]);
         }
+
+        negativeMap[nums[i]]++;
       }
     }
 
-    int pLength = positiveMap.size();
-    int nLength = negativeMap.size();
+    int pLength = positiveNums.size();
+    int nLength = negativeNums.size();
 
     if (zeroLength >= 3) results.push_back({0, 0, 0});
 
     if (pLength == 0 || nLength == 0) return results;
 
     bool isPositiveSmaller = pLength < nLength;
+    bool hasZero = zeroLength > 0;
 
-    unordered_map<int, int>::iterator begin1 = positiveMap.begin();
-    unordered_map<int, int>::iterator end = positiveMap.end();
-
-    while (begin1 != end) {
-      if (zeroLength != 0 && isPositiveSmaller) {
-        int complement = 0 - begin1->first;
+    for (int i = 0; i < pLength; i++) {
+      if (hasZero && isPositiveSmaller) {
+        int complement = 0 - positiveNums[i];
 
         if (negativeMap.count(complement)) {
-          results.push_back({complement, 0, begin1->first});
+          results.push_back({complement, 0, positiveNums[i]});
         }
       }
 
-      if (begin1->second >= 2) {
-        int complement = 0 - begin1->first - begin1->first;
+      if (positiveMap[positiveNums[i]] >= 2) {
+        int complement = 0 - positiveNums[i] - positiveNums[i];
 
         if (negativeMap.count(complement)) {
-          results.push_back({complement, begin1->first, begin1->first});
+          results.push_back({complement, positiveNums[i], positiveNums[i]});
         }
       }
 
-      unordered_map<int, int>::iterator begin2 = begin1;
-      begin2++;
-
-      while (begin2 != end) {
-        int complement = 0 - begin1->first - begin2->first;
+      for (int j = i + 1; j < pLength; j++) {
+        int complement = 0 - positiveNums[i] - positiveNums[j];
 
         if (negativeMap.count(complement)) {
-          results.push_back({complement, begin1->first, begin2->first});
+          results.push_back({complement, positiveNums[i], positiveNums[j]});
         }
-
-        begin2++;
       }
-
-      begin1++;
     }
 
-    begin1 = negativeMap.begin();
-    end = negativeMap.end();
-
-    while (begin1 != end) {
-      if (zeroLength != 0 && !isPositiveSmaller) {
-        int complement = 0 - begin1->first;
+    for (int i = 0; i < nLength; i++) {
+      if (hasZero && !isPositiveSmaller) {
+        int complement = 0 - negativeNums[i];
 
         if (positiveMap.count(complement)) {
-          results.push_back({begin1->first, 0, complement});
+          results.push_back({complement, 0, negativeNums[i]});
         }
       }
 
-      if (begin1->second >= 2) {
-        int complement = 0 - begin1->first - begin1->first;
+      if (negativeMap[negativeNums[i]] >= 2) {
+        int complement = 0 - negativeNums[i] - negativeNums[i];
 
         if (positiveMap.count(complement)) {
-          results.push_back({begin1->first, begin1->first, complement});
+          results.push_back({complement, negativeNums[i], negativeNums[i]});
         }
       }
 
-      unordered_map<int, int>::iterator begin2 = begin1;
-      begin2++;
-
-      while (begin2 != end) {
-        int complement = 0 - begin1->first - begin2->first;
+      for (int j = i + 1; j < nLength; j++) {
+        int complement = 0 - negativeNums[i] - negativeNums[j];
 
         if (positiveMap.count(complement)) {
-          results.push_back({begin1->first, begin2->first, complement});
+          results.push_back({complement, negativeNums[i], negativeNums[j]});
         }
-
-        begin2++;
       }
-
-      begin1++;
     }
 
     return results;
@@ -130,23 +119,19 @@ class Solution {
 
 int main() {
   vector<int> nums = {-1, 0, 1, 2, -1, -4};
-
   vector<vector<int>> results = solution.threeSum(nums);
-  cout << "{ " << endl;
 
+  cout << "{ " << endl;
   for (int i = 0; i < results.size(); i++) {
     cout << "  { ";
-
     for (int j = 0; j < results[i].size(); j++) {
       cout << results[i][j];
       if (j != results[i].size() - 1) {
         cout << ", ";
       }
     }
-
     cout << " }," << endl;
   }
-
   cout << "}" << endl;
 
   return 0;
---
+++ leetcode/49_group_anagrams.cxx
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+/**
+ * 直覺上，質數 hash 就可以解了，
+ * 如果 overflow ，可以多分幾段，
+ * T1 解是另一種神奇的 hash ，
+ * 
+ * 另外發現 unordered_map::begin 好像很慢耶
+ * 蠻奇怪的
+ * 
+ * TODO: 有空研究一下那個 hash 
+ */
+
+int primeNums[] = {2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37,  41,  43,
+                   47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107};
+
+class Solution {
+ public:
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> results;
+    unordered_map<unsigned int, vector<string>> map;
+    vector<unsigned int> keys;
+
+    int length = strs.size();
+
+    for (int i = 0; i < length; i++) {
+      unsigned int key = getHash(strs[i]);
+
+      if (!map.count(key)) {
+        map[key] = {};
+        keys.push_back(key);
+      }
+
+      map[key].push_back(strs[i]);
+    }
+
+    length = keys.size();
+
+    for (int i = 0; i < length; i++) {
+      results.push_back(map[keys[i]]);
+    }
+
+    return results;
+  }
+
+  unsigned int getHash(string str) {
+    int length = str.size();
+    unsigned int result = 1;
+
+    for (int i = 0; i < length; i++) {
+      result *= primeNums[str[i] - 'a'];
+    }
+
+    return result;
+  }
+};
