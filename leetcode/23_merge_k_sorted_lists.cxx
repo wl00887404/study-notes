42707d8bbe497d46d9d454406185df033a86b547
wl00887404 <wl00887404@gmail.com>
2020-04-28 20:38:12 +0800

    23: 兩兩合併解

--- leetcode/23_merge_k_sorted_lists.cxx
+++ leetcode/23_merge_k_sorted_lists.cxx
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+
+#include "./log.cxx"
+
 using namespace std;
 
 /**
@@ -10,7 +13,15 @@ using namespace std;
  * testcase 應該 lists.size() 非常大吧
  *
  * 用 unordered_map 將同樣的 val 的目標先串起來: 36 ms
- * 全部先讀取出來: 32 ms
+ * 全部讀取出來: 32 ms
+ *
+ * unordered_map 與全部讀出來
+ * 時間差不多 效能應該差在排序上
+ *
+ * 用 insert 的不會比較快 慘慘慘
+ *
+ * T1 解是兩個兩個合併耶
+ * 但測試之後也是 32ms 應該也是 O(nlog n)
  */
 
 struct ListNode {
@@ -21,46 +32,45 @@ struct ListNode {
 
 class Solution {
  public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode head = ListNode(0);
+    ListNode* p = &head;
+
+    while (l1 != NULL && l2 != NULL) {
+      if (l1->val < l2->val) {
+        p->next = l1;
+        l1 = l1->next;
+      } else {
+        p->next = l2;
+        l2 = l2->next;
+      };
+
+      p = p->next;
+    }
+
+    if (l1 != NULL && l2 == NULL) {
+      p->next = l1;
+    } else if (l1 == NULL && l2 != NULL) {
+      p->next = l2;
+    }
+
+    return head.next;
+  }
   ListNode* mergeKLists(vector<ListNode*>& lists) {
     int length = lists.size();
 
     if (length == 0) return 0;
     if (length == 1) return lists[0];
 
-    ListNode head = ListNode(0);
-    vector<int> keys;
-    unordered_map<int, ListNode*> headMap;  // (val, head)
-    unordered_map<int, ListNode*> lastMap;  // (val, last)
-    bool isFinished = false;
-
-    for (int i = 0; i < length; i++) {
-      while (lists[i] != NULL) {
-        ListNode* originalNext = lists[i]->next;
-
-        if (headMap.count(lists[i]->val)) {
-          lists[i]->next = headMap[lists[i]->val];
-          headMap[lists[i]->val] = lists[i];
-        } else {
-          headMap[lists[i]->val] = lists[i];
-          lastMap[lists[i]->val] = lists[i];
-          keys.push_back(lists[i]->val);
-        }
-
-        lists[i] = originalNext;
+    while (length != 1) {
+      for (int i = 0; i < length / 2; i++) {
+        lists[i] = mergeTwoLists(lists[i], lists[length - i - 1]);
       }
+
+      length = length / 2 + length % 2;
     }
 
-    sort(keys.begin(), keys.end());
-    ListNode* p = &head;
-
-    for (int key : keys) {
-      p->next = headMap[key];
-      p = lastMap[key];
-    }
-
-    p->next = NULL;
-
-    return head.next;
+    return lists[0];
   }
 } solution;
 
@@ -73,54 +83,55 @@ class ReadAllSolution {
     if (length == 1) return lists[0];
 
     vector<int> values;
+    ListNode head = ListNode(0);
+    ListNode* p = &head;
 
     for (int i = 0; i < length; i++) {
       while (lists[i] != NULL) {
         values.push_back(lists[i]->val);
+        p->next = lists[i];
+        p = p->next;
         lists[i] = lists[i]->next;
       }
     }
 
-    ListNode head = ListNode(0);
     sort(values.begin(), values.end());
-    ListNode* p = &head;
+    p = &head;
 
     for (int value : values) {
-      p->next = new ListNode(value);
       p = p->next;
+      p->val = value;
     }
-
-    p->next = NULL;
 
     return head.next;
   }
 };
 
 int main() {
-  // ListNode* l1 = new ListNode(1);
-  // l1->next = new ListNode(4);
-  // l1->next->next = new ListNode(5);
-
-  // ListNode* l2 = new ListNode(1);
-  // l2->next = new ListNode(3);
-  // l2->next->next = new ListNode(4);
-
-  // ListNode* l3 = new ListNode(2);
-  // l3->next = new ListNode(6);
-
-  // vector<ListNode*> lists = {l1, l2, l3};
-  // ListNode* p = solution.mergeKLists(lists);
-
   ListNode* l1 = new ListNode(1);
-  l1->next = new ListNode(2);
-  l1->next->next = new ListNode(2);
+  l1->next = new ListNode(4);
+  l1->next->next = new ListNode(5);
 
   ListNode* l2 = new ListNode(1);
-  l2->next = new ListNode(1);
-  l2->next->next = new ListNode(2);
+  l2->next = new ListNode(3);
+  l2->next->next = new ListNode(4);
 
-  vector<ListNode*> lists = {l1, l2};
+  ListNode* l3 = new ListNode(2);
+  l3->next = new ListNode(6);
+
+  vector<ListNode*> lists = {l1, l2, l3};
   ListNode* p = solution.mergeKLists(lists);
+
+  // ListNode* l1 = new ListNode(1);
+  // l1->next = new ListNode(2);
+  // l1->next->next = new ListNode(2);
+
+  // ListNode* l2 = new ListNode(1);
+  // l2->next = new ListNode(1);
+  // l2->next->next = new ListNode(2);
+
+  // vector<ListNode*> lists = {l1, l2};
+  // ListNode* p = solution.mergeKLists(lists);
 
   while (p != NULL) {
     cout << p->val << endl;
