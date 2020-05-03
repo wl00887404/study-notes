c87e70315e254930dc83e150558cbd945f00785e
wl00887404 <wl00887404@gmail.com>
2020-05-03 17:21:17 +0800

    383: 上面有巫術

--- leetcode/383_ransom_note.cxx
+++ leetcode/383_ransom_note.cxx
@@ -3,33 +3,32 @@
 #include "./log.cxx"
 using namespace std;
 
+static vector<int> nums = []() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  return vector<int>{};
+}();
+
 class Solution {
  public:
   bool canConstruct(string ransomNote, string magazine) {
-    int ransomNoteLength = ransomNote.size();
+    int rLength = ransomNote.size();
 
-    if (ransomNoteLength == 0) return true;
+    if (rLength == 0) return true;
 
-    int magazineLength = magazine.size();
+    int mLength = magazine.size();
 
-    if (magazineLength == 0) return false;
+    if (mLength == 0) return false;
 
     int frequencies[26] = {0};
 
-    for (int i = 0; i < ransomNoteLength; i++) {
-      frequencies[ransomNote[i] - 'a']++;
+    for (int i = 0; i < mLength; i++) frequencies[magazine[i] - 'a']++;
+
+    for (int i = 0; i < rLength; i++) {
+      if (--frequencies[ransomNote[i] - 'a'] < 0) return false;
     }
 
-    for (int i = 0; i < magazineLength; i++) {
-      int index = magazine[i] - 'a';
-      if (frequencies[index] == 0) continue;
-      frequencies[index]--;
-      ransomNoteLength--;
-
-      if (ransomNoteLength == 0) return true;
-    }
-
-    return false;
+    return true;
   }
 } solution;
 
