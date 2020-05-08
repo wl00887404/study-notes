12e4688733f20105538e0407f434d42615c5bdfc
wl00887404 <wl00887404@gmail.com>
2020-05-09 00:13:04 +0800

    98: binary 系列都寫得不是很好

--- leetcode/54_spiral_matrix.cxx
+++ leetcode/54_spiral_matrix.cxx
@@ -4,19 +4,6 @@
 #include "./log.cxx"
 using namespace std;
 
-/**
- *  1 11
- *  2 12
- *  3 13
- *  4 14
- *  5 15
- *  6 16
- *  7 17
- *  8 18
- *  9 19
- * 10 20
- */
-
 class Solution {
  public:
   vector<int> spiralOrder(vector<vector<int>>& matrix) {
---
+++ leetcode/98_validate_binary_search_tree.cxx
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "./log.cxx"
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isValidBST(TreeNode *root) {
+    if (root == NULL) return true;
+
+    if (!isChildValid(root->left, NULL, &root->val)) return false;
+
+    return isChildValid(root->right, &root->val, NULL);
+  }
+
+  bool isChildValid(TreeNode *node, int *min, int *max) {
+    if (node == NULL) return true;
+
+    if (min != NULL && *min >= node->val) return false;
+    if (max != NULL && node->val >= *max) return false;
+
+    if (!isChildValid(node->left, min, &node->val)) return false;
+
+    return isChildValid(node->right, &node->val, max);
+  }
+} solution;
+
+class T1Solution {
+ public:
+  int *last = NULL;
+  bool isValidBST(TreeNode *root) {
+    if (root == NULL) return true;
+
+    if (!isValidBST(root->left)) return false;
+    if (last && *last >= root->val) return false;
+    last = &root->val;
+    if (!isValidBST(root->right)) return false;
+    return true;
+  };
+};
+
+int main() {
+  TreeNode *root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+  cout << solution.isValidBST(root) << endl;
+
+  return 0;
+}
+
+/**
+ *  3
+ *  1                5
+ *  0         2      4 6
+ *  null null null 3
+ */
