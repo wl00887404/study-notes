bfe2313e11fec4a6c57dc48201f0bb4620feda8e
wl00887404 <wl00887404@gmail.com>
2020-05-19 03:39:17 +0800

    567: 不重來

--- leetcode/567_permutation_in_string.cxx
+++ leetcode/567_permutation_in_string.cxx
@@ -1,37 +1,63 @@
 #include <iostream>
 using namespace std;
 
-/**
- * 超慢
- */
-
 class Solution {
  public:
   bool checkInclusion(string s1, string s2) {
     int length1 = s1.size();
     int length2 = s2.size();
     int charMap1[26] = {0};
+    int charMap2[26] = {0};
 
     for (int i = 0; i < length1; i++) charMap1[s1[i] - 'a']++;
 
     int limit = length2 - length1 + 1;
+    int i = 0;
+    int j = 0;
 
-    for (int i = 0; i < limit; i++) {
-      int j = 0;
-      int charMap2[26] = {0};
+    while (i < limit) {
+      if (j >= length1) return true;
 
-      while (true) {
-        if (j >= length1) return true;
+      int ci = s2[i + j] - 'a';
 
-        int ci = s2[i + j] - 'a';
+      if (charMap1[ci] == 0) {
+        if (j < 26) {
+          for (int k = 0; k < j; k++) charMap2[s2[i + k] - 'a']--;
+        } else {
+          for (int k = 0; k < 26; k++) charMap2[k] = 0;
+        }
 
-        if (charMap1[ci] == 0 || charMap2[ci] == charMap1[ci]) break;
+        i += j + 1;
+        j = 0;
 
-        charMap2[ci]++;
-        j++;
+        continue;
       }
+
+      if (charMap1[ci] == charMap2[ci]) {
+        int k = 0;
+
+        while (s2[i + k] != s2[i + j]) {
+          charMap2[s2[i + k] - 'a']--;
+          k++;
+        }
+
+        i += k + 1;
+        j -= k;
+
+        continue;
+      }
+
+      charMap2[ci]++;
+      j++;
     }
 
     return false;
   }
-};
+} solution;
+
+int main() {
+  // cout << solution.checkInclusion("hello", "ooolleoooleh") << endl;
+  cout << solution.checkInclusion("ab", "eidbaooo") << endl;
+
+  return 0;
+}
