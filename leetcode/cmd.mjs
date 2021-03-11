758ba7a883d7bbb6c5560db58aad901ddd668df3
Max He <wl00887404@gmail.com>
2021-03-11 12:20:30 +0800

    :sparkles: 新增 leetcode update 的 script

--- leetcode/cmd.mjs
+++ leetcode/cmd.mjs
@@ -1,16 +1,24 @@
 import axios from 'axios';
 import { resolve } from 'path';
-import { appendFileSync } from 'fs';
+import { readFileSync, writeFileSync, appendFileSync } from 'fs';
 
 const scriptPath = process.argv[1];
 const action = process.argv[2];
+const questionsPath = resolve(scriptPath, `../questions.json`);
 
 const main = async () => {
+  if (action === 'update') {
+    const { data } = await axios.get('https://leetcode.com/api/problems/all');
+
+    return writeFileSync(questionsPath, JSON.stringify(data.stat_status_pairs));
+  }
   if (action === 'add') {
     const questionId = parseInt(process.argv[3]);
+    const questions = JSON.parse(
+      readFileSync(questionsPath, { encoding: 'utf-8' }),
+    );
 
-    const { data } = await axios.get('https://leetcode.com/api/problems/all');
-    const question = data.stat_status_pairs.find(
+    const question = questions.find(
       question => question.stat.frontend_question_id === questionId,
     );
 
