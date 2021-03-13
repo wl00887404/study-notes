65aa99b5d2bba79b817c72b443877cd69883f7fb
Max He <wl00887404@gmail.com>
2021-03-13 17:04:27 +0800

    :sparkles: 139: 加入 cache 終於過了

--- leetcode/139_word_break.cxx
+++ leetcode/139_word_break.cxx
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 /**
- * Trie 解超時 QAQ
+ * 使用 Trie: 3cabe0d
+ * 刪去字典重複組合: q1e16f73
  */
+
+using namespace std;
 
 struct Trie {
   Trie* children[26] = {NULL};
   bool isWord = false;
-  bool isReachable = false;
 
   Trie() {}
 };
@@ -29,20 +30,6 @@ void append(string::iterator begin, string::iterator end, Trie* pointer) {
     append(begin + 1, end, pointer->children[index]);
   }
 }
-
-bool find(string::iterator begin, string::iterator end, Trie* head,
-          Trie* pointer) {
-  if (pointer == NULL) return false;
-  pointer->isReachable = true;
-
-  if (begin == end) return pointer->isWord;
-
-  if (pointer->isWord && find(begin, end, head, head)) return true;
-
-  int index = getIndex(begin);
-  return find(begin + 1, end, head, pointer->children[index]);
-}
-
 class Solution {
  public:
   bool wordBreak(string s, vector<string>& wordDict) {
@@ -51,34 +38,28 @@ class Solution {
       append(word.begin(), word.end(), &head);
     }
 
-    for (string& word : wordDict) {
-      find(word.begin(), word.end(), &head, &head);
-    }
+    int length = s.length();
+    bool failureArray[length];
+    for (int i = 0; i < length; i++) failureArray[i] = false;
 
-    return wordBreak(s.begin(), s.end(), &head, &head);
+    return wordBreak(s.begin(), s.end(), failureArray, &head, &head);
   }
-  bool wordBreak(string::iterator begin, string::iterator end, Trie* head,
-                 Trie* pointer) {
+
+  bool wordBreak(string::iterator begin, string::iterator end,
+                 bool* failPointer, Trie* head, Trie* pointer) {
     if (pointer == NULL) return false;
-    if (!pointer->isReachable) return false;
+    if (pointer == head && *failPointer == true) return false;
     if (begin == end) return pointer->isWord;
 
     int index = getIndex(begin);
-    if (pointer->isWord && wordBreak(begin, end, head, head)) return true;
-
-    return wordBreak(begin + 1, end, head, pointer->children[index]);
+    if (pointer->isWord && wordBreak(begin, end, failPointer, head, head)) {
+      return true;
+    }
+    if (wordBreak(begin + 1, end, failPointer + 1, head,
+                  pointer->children[index])) {
+      return true;
+    }
+    if (pointer == head) *failPointer = true;
+    return false;
   }
 } solution;
-
-int main() {
-  string s =
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-      "aaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-      "aaaaaaa";
-  vector<string> wordDict = {"aa",         "aaa",     "aaaa",     "aaaaa",
-                             "aaaaaa",     "aaaaaaa", "aaaaaaaa", "aaaaaaaaa",
-                             "aaaaaaaaaa", "ba"};
-
-  cout << solution.wordBreak(s, wordDict) << endl;
-  return 0;
-}
