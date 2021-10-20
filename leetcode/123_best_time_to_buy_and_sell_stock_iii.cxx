48c06f6ae912b38c232d1cd94bbad63ecfb59d4f
Max He <wl00887404@gmail.com>
2021-10-20 21:53:30 +0800

    :sparkles: 123: 超時 QQ

---
+++ leetcode/123_best_time_to_buy_and_sell_stock_iii.cxx
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/**
+ * 規則改成只有兩次交易機會
+ * 一樣不能買兩筆等著賣
+ * 與 309 不同是沒有 CD
+ */
+
+/**
+ * 只能交易兩次 = 前段最佳 + 後段最佳
+ * 假設有 7 項
+ * 結果為
+ *   前 2 項 + 後 5 項
+ *   前 3 項 + 後 4 項
+ *   前 4 項 + 後 3 項
+ *   前 5 項 + 後 2 項
+ *   前 6 項（只進行一次交易）
+ *   後 6 項（只進行一次交易）
+ *   整列（只進行一次交易）
+ *
+ *   前 1 項 h0 = 0
+ *   前 2 項 h1 = max(h0, a1 - a0)
+ *   前 3 項 h2 = max(h1, a2 - a0, a2 - a1)
+ *   前 4 項 h3 = max(h2, a3 - a0, a3 - a1, a3 - a2)
+ *   前 5 項 h4 = max(h3, a4 - a0, a4 - a1, a4 - a2, a4 - a3)
+ *   前 6 項 h5 = max(h3, a5 - a0, a5 - a1, a5 - a2, a5 - a3, a5 - a4)
+ *
+ *   後 1 項 t6 = 0
+ *   後 2 項 t5 = max(t6, a6 - a5)
+ *   後 3 項 t4 = max(t5, a6 - a4, a5 - a4)
+ *   後 4 項 t3 = max(t4, a6 - a3, a5 - a3, a4 - a3)
+ *   後 5 項 t2 = max(t3, a6 - a2, a5 - a2, a4 - a2, a3 - a2)
+ *   後 6 項 t1 = max(t2, a6 - a1, a5 - a1, a4 - a1, a3 - a1, a2 - a1)
+ */
+
+void log(vector<int>& array) {
+  for (int i = 0; i < array.size(); i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void log(int* array, int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices) {
+    int size = prices.size();
+    int result = 0;
+    int h[size];
+    int t[size];
+
+    h[0] = 0;
+    for (int i = 1; i < size; i++) {
+      h[i] = h[i - 1];
+      for (int j = 0; j < i; j++) {
+        h[i] = max(h[i], prices[i] - prices[j]);
+      }
+
+      if (h[i] > result) result = h[i];
+    }
+
+    t[size - 1] = 0;
+    for (int i = size - 2; 0 <= i; i--) {
+      t[i] = t[i + 1];
+      for (int j = size - 1; i < j; j--) {
+        t[i] = max(t[i], prices[j] - prices[i]);
+      }
+      if (t[i] > result) result = t[i];
+    }
+
+    for (int i = 1; i < size - 1; i++) {
+      if (h[i] == 0 || t[i + 1] == 0) continue;
+      result = max(result, h[i] + t[i + 1]);
+    }
+
+    // log(prices);
+    // log(h, size);
+    // log(t, size);
+    // cout << result << endl;
+
+    return result;
+  }
+} solution;
+
+int main() {
+  // vector<int> prices = {7, 6, 5, 4, 3, 4, 5};
+  vector<int> prices = {545, 544, 543, 542, 541, 540, 539, 541, 540, 542,
+                        539, 538, 537, 536, 535, 534, 533, 532, 531, 530,
+                        529, 528, 527, 526, 525, 0,   0,   0,   0,   0,
+                        0,   0,   0,   0,   0,   0,   0,   0,   0,   0};
+
+  solution.maxProfit(prices);
+
+  return 0;
+}
