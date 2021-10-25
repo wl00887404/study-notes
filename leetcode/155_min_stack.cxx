65a9502d26db99cb992ad3bc9a9898d6de25f181
Max He <wl00887404@gmail.com>
2021-10-25 21:20:33 +0800

    :sparkles: 155: 看了討論區才解出來

--- leetcode/155_min_stack.cxx
+++ leetcode/155_min_stack.cxx
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <stack>
+
 using namespace std;
 
 /**
@@ -12,9 +13,42 @@ using namespace std;
  * 這樣怎麼會是 O(1)
  */
 
+/**
+ * 其實用一個 stack 存目前的值
+ * 再用一個 stack 存最小值
+ * 這樣就解了
+ */
+
 class MinStack {
  public:
   MinStack() {}
+
+  void push(int val) {
+    valStack.push(val);
+    if (minStack.empty() || minStack.top() > val) {
+      minStack.push(val);
+    } else {
+      minStack.push(minStack.top());
+    }
+  }
+
+  void pop() {
+    valStack.pop();
+    minStack.pop();
+  }
+
+  int top() { return valStack.top(); }
+
+  int getMin() { return minStack.top(); }
+
+ private:
+  stack<int> valStack;
+  stack<int> minStack;
+};
+
+class MyMinStack {
+ public:
+  MyMinStack() {}
 
   void push(int val) {
     myStack.push(val);
